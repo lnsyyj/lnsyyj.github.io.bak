64f51e1125a89c53db7ac4fa2f04583d226e33e8
lnsyyj <lnsyyj@hotmail.com>
2019-06-06 15:11:02 +0800

    update
    Signed-off-by: lnsyyj <lnsyyj@hotmail.com>

--- Blog/source/_posts/Ceph数据正确性与一致性守护者Scrub读书笔记/ceph13.2.4关于scrub配置项.cpp
+++ Blog/source/_posts/Ceph数据正确性与一致性守护者Scrub读书笔记/ceph13.2.4关于scrub配置项.cpp
@@ -1,16 +1,3 @@
----
-title: Ceph数据正确性与一致性守护者Scrub读书笔记
-date: 2019-06-03 16:17:02
-tags: Ceph
----
-
-![](https://raw.githubusercontent.com/lnsyyj/lnsyyj.github.io/hexo/Blog/source/_posts/Ceph%E6%95%B0%E6%8D%AE%E6%AD%A3%E7%A1%AE%E6%80%A7%E4%B8%8E%E4%B8%80%E8%87%B4%E6%80%A7%E5%AE%88%E6%8A%A4%E8%80%85Scrub%E8%AF%BB%E4%B9%A6%E7%AC%94%E8%AE%B0/Scrub.png)
-
-
-
-Ceph关于Scrub相关配置
-
-```
 OPTION(mon_warn_not_scrubbed, OPT_INT)
 OPTION(mon_warn_not_deep_scrubbed, OPT_INT)
 OPTION(mon_scrub_interval, OPT_INT) // once a day
@@ -51,21 +38,3 @@ OPTION(osd_debug_deep_scrub_sleep, OPT_FLOAT)
 OPTION(osd_scrub_priority, OPT_U32)
 OPTION(osd_scrub_cost, OPT_U32) // set default cost equal to 50MB io
 OPTION(osd_requested_scrub_priority, OPT_U32)	// set requested scrub priority higher than scrub priority to make the requested scrubs jump the queue of scheduled scrubs
-```
-
-
-
-临时结论：
-
-1、目前无法避免静默错误，Scrub也无法修复严重的静默错误
-
-2、Scrub是Ceph检测静默错误的机制
-
-3、目前发现静默错误也没有好的方法修复（只能通知客户尽可能挽回数据）
-
-
-
-想法：
-
-1、借助Scrub作为测试正确性与一致性的工具（需要解决覆盖写问题）
-
